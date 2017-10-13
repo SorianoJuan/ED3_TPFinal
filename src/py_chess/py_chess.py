6fddfe0a3df379f36a35c9af092389b25b76dff1
ivanvig <ivanmvig@gmail.com>
2017-10-13 20:11:09 -0300

    imagen actualizandose

--- src/py_chess/py_chess.py
+++ src/py_chess/py_chess.py
@@ -1,14 +1,41 @@
-import chess.svg
-from wand.image import Image
-from wand.color import Color
-from wand import display
+import sys
+from PyQt5 import QtCore, QtWidgets, QtSvg
 
-board = chess.Board()
 
-img_svg = chess.svg.board(board=board)
+class Main(QtWidgets.QMainWindow):
 
-with Image(blob=img_svg.encode(), format='svg', resolution=175) as img:
-    img.background_color = Color('white')
-    img.alpha_channel = 'remove'
-    display.display(img)
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.timer = QtCore.QTimer(self)
+        self.svgW = QtSvg.QSvgWidget()
+        self.initUI()
+
+    def initUI(self):
+
+        self.timer.timeout.connect(self.Refresh)
+        self.timer.start(150)
+
+        self.svgW.load('asd')
+
+        self.setCentralWidget(self.svgW)
+
+        #---------Window settings --------------------------------
+
+        self.setGeometry(50, 50, 750, 750)
+        self.setWindowTitle("Chess.ini")
+
+    #-------- Slots ------------------------------------------
+
+    def Refresh(self):
+        self.svgW.load('asd')
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = Main()
+    main.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
 
